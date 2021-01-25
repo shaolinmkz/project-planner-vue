3fb3f8d46fe84ef4c1de845f368d20f2df736825
shaolinmkz007 <emeka@softcom.ng>
2021-01-26 00:50:32 +0100

    implement project filter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
+  <FilterNav :current="filterValue" @filterChange="handleFilterEvent" />
   <div class="home">
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject
           :project="project"
           @deleteProject="handleDeleteProject"
@@ -15,25 +16,55 @@
 
 <script>
 import SingleProject from "../components/SingleProject.vue";
+import FilterNav from "../components/FilterNav.vue";
 
 export default {
   name: "Home",
   components: {
     SingleProject,
+    FilterNav,
   },
   data() {
     return {
+      // backupProjects: [],
       projects: [],
+      filterValue: "all",
     };
   },
   methods: {
     handleDeleteProject(projectId) {
       this.projects = this.projects.filter(({ id }) => id !== projectId);
+      // this.backupProjects = this.projects;
     },
     handleCompleteProject(singleProject) {
       this.projects = this.projects.map((project) =>
         project.id === singleProject.id ? singleProject : project
       );
+      // this.backupProjects = this.projects;
+    },
+    handleFilterEvent(by) {
+      this.filterValue = by;
+    },
+    // handleFilterProject(by) {
+    //   this.filterValue = by;
+
+    //   if (by === "all") {
+    //     this.projects = this.backupProjects;
+    //   } else if (by === "ongoing") {
+    //     this.projects = this.backupProjects.filter(({ complete }) => !complete);
+    //   } else if (by === "completed") {
+    //     this.projects = this.backupProjects.filter(({ complete }) => complete);
+    //   }
+    // },
+  },
+  computed: {
+    filteredProjects() {
+      if (this.filterValue === "ongoing") {
+        return this.projects.filter(({ complete }) => !complete);
+      } else if (this.filterValue === "completed") {
+        return this.projects.filter(({ complete }) => complete);
+      }
+      return this.projects;
     },
   },
   mounted() {
@@ -41,6 +72,7 @@ export default {
       .then((res) => res.json())
       .then((data) => {
         this.projects = data;
+        this.backupProjects = data;
       })
       .catch(({ message }) => console.log(message));
   },
